6b34a4ba8491e591ea4c09ddf74049dca24863a2
Piyush Khemka <piyushkhemka123@gmail.com>
2016-05-29 18:20:26 +0530

    More solutions

---
+++ Search a 2d array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        
+        int low = 0; int high = row*col - 1;
+        
+        while(low<=high) {
+            int mid = (low + high)/2;
+            if(matrix[mid/col][mid%col] ==  target)
+                return true;
+            else if (matrix[mid/col][mid%col] > target)
+                high = mid - 1;
+            else low = mid + 1;
+        }
+        return false;
+    }
+};
