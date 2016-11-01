474318141e3eb04c1f3b5ffdd6844029f75aeeb9
Piyush <piyushkhemka123@gmail.com>
2016-11-01 15:12:13 -0400

    revisited some old problems

---
+++ 110. Balanced Binary Tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+
+private:
+
+    int height(TreeNode *root) {
+
+        if(!root)
+            return 0;
+
+        return 1 + max(height(root->left),height(root->right));
+    }
+
+public:
+    bool isBalanced(TreeNode* root) {
+
+        if (root == NULL)
+            return true;
+
+        return (abs(height(root->left) - height(root->right)) < 2) && isBalanced(root->left) && isBalanced(root->right);
+
+    }
+};
